be06b9d9129220c27a6a79e9d0556b4e0aa6e7ea
EmilVattai <vattai.emil@gmail.com>
2025-05-09 17:43:00 +0200

    findPartner function done, parodystring issue fixed

--- findPartner.js
+++ findPartner.js
@@ -1,5 +1,14 @@
 function findPartner(customer, candidates) {
-  //write your code here
+  const foundCandidates = candidates.filter(candidate => {
+    return candidate.favoriteGenre === customer.favoriteGenre && 
+      candidate.hobbies.some(hobby => customer.hobbies.includes(hobby)); 
+  });
+  if (foundCandidates.length === 0) {
+    return null;
+  } else {
+    const sortedCandidates = foundCandidates.sort((a, b) => a.age - b.age);
+    return sortedCandidates[0];
+  }
 }
 
 module.exports = findPartner
--- parodyString.js
+++ parodyString.js
@@ -1,8 +1,10 @@
 function parodyString(string) {
   const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   return string.split('').map((char, index) => {
-    if (letters.includes(char)) return index % 2 ? char.toLowerCase() : char.toUpperCase();
+    if (letters.includes(char)) return index % 2 ? char.toLowerCase() : char.toUpperCase(); else return char;
   }).join('');
 }
+
+console.log(parodyString("Hello World!"))
 
 module.exports = parodyString
